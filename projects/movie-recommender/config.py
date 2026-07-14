69cc039210b0aa112808c41eeffa2c3263cc077f
Abhishek Agrawal <godabhi813@gmail.com>
2026-07-14 17:08:53 +0530

    fix: use absolute paths based on config.py location instead of relative paths

--- projects/movie-recommender/config.py
+++ projects/movie-recommender/config.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
+# Base directory — jaha config.py khud hai, uske hisaab se path banayenge
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # local paths of files 
 MOVIE_LIST_PATH = "models/movies.pkl"
@@ -12,7 +14,7 @@ SIMILARITY_PATH = "models/similarity.pkl"
 
 # google drive link of similarity_path as its large 
 
-SIMILARITY_GDRIVE_URL = "https://drive.google.com/file/d/17V0O1wAl2yDsQQZ6nL7oI99yYaFNiFiZ/view?usp=drive_link"
+SIMILARITY_GDRIVE_URL = "https://drive.google.com/uc?id=17V0O1wAl2yDsQQZ6nL7oI99yYaFNiFiZ"
 
 # TMDB API base URLs
 TMDB_BASE_URL = "https://api.themoviedb.org/3/movie"
